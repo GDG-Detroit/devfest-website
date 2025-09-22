123c231a91ff6acf9436803c2bc509937200cd22
Greg Miller <hello@gregmiller.io>
2025-09-22 13:17:59 -0400

    refactor: remove excess spacing

--- src/components/ui/GithubHandle.jsx
+++ src/components/ui/GithubHandle.jsx
@@ -6,9 +6,9 @@ const GithubHandle = ({ handle }) => (
     href={handle}
     target="_blank"
     rel="noreferrer"
-    className="absolute right-0 top-0 z-10 flex h-[34px] w-[34px] -translate-x-1/2 -translate-y-1/2  items-center justify-center rounded-full bg-black"
+    className="absolute right-0 top-0 z-10 flex h-[34px] w-[34px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-black"
   >
-    <FaGithub className=" text-white" />
+    <FaGithub className="text-white" />
   </a>
 )
 
