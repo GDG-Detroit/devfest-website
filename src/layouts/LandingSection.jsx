b48236dd7881ccc9c00c3126b4e9e7b4ed985879
Greg Miller <hello@gregmiller.io>
2025-09-22 13:19:05 -0400

    refactor: fix classname order
    - resolve issue with eslint identifying bugs
    - need to reorder manually

--- src/layouts/LandingSection.jsx
+++ src/layouts/LandingSection.jsx
@@ -10,7 +10,7 @@ function LandingSection() {
   return (
     <section id="landing" className="mt-16">
       <div className="flex flex-wrap bg-black">
-        <div className="p-12 w-full sm:w-1/2">
+        <div className="w-full p-12 sm:w-1/2">
           <ResponsiveImage
             className="w-full"
             webpSrc={DevFestLogoWebp}
@@ -19,8 +19,8 @@ function LandingSection() {
           />
         </div>
 
-        <div className="flex flex-col justify-between w-full bg-primary-400 sm:w-1/2">
-          <div className="flex flex-col justify-center items-center p-10 space-y-8 grow">
+        <div className="flex w-full flex-col justify-between bg-primary-400 sm:w-1/2">
+          <div className="flex grow flex-col items-center justify-center space-y-8 p-10">
             <ResponsiveImage
               className="w-[570px] max-w-full"
               webpSrc={dateWebp}
@@ -28,7 +28,7 @@ function LandingSection() {
               alt="Date and Red Circle"
             />
 
-            <p className="text-sm font-normal text-justify lg:text-base xl:text-lg">
+            <p className="text-justify text-sm font-normal lg:text-base xl:text-lg">
               The Detroit and Ann Arbor Google Developer Groups (GDG) are
               teaming up once again to bring you the 10th Michigan DevFest on
               November 22, 2025, at Motor City Casino. With our 11th DevFest, we
@@ -38,7 +38,7 @@ function LandingSection() {
             <a
               href="https://www.papercall.io/midevfest2025"
               target="_blank"
-              className="flex items-center px-8 py-5 bg-sky-900 rounded shadow-xl transition duration-100 ease-in-out delay-75 text-primary-400 hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"
+              className="flex items-center rounded bg-sky-900 px-8 py-5 text-primary-400 shadow-xl transition delay-75 duration-100 ease-in-out hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"
               rel="noreferrer"
             >
               {' '}
