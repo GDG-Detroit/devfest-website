9f5b2d86e43456775bfc9caa70f6ddddb7145735
Greg Miller <hello@gregmiller.io>
2025-09-29 11:23:59 -0400

    refactor: make address accessible
    - use address element
    - add get directions button for cleaner hit target

--- src/layouts/LocationSection.jsx
+++ src/layouts/LocationSection.jsx
@@ -31,17 +31,26 @@ function LocationSection() {
               Where
             </h3>
           </div>
-          <a
-            href="https://www.google.com/maps/dir//2901+Grand+River+Ave,+Detroit,+MI+48201/@42.3388889,-83.0703527,1162m/data=!3m2!1e3!5s0x883b2d527a607d6f:0xe075cfc124b83793!4m20!1m10!3m9!1s0x883b2d4d71da8bb1:0xe4a8072514ccbe55!2sMotorCity+Casino+Hotel!5m2!4m1!1i2!8m2!3d42.3388889!4d-83.0677778!16zL20vMDdtMmN3!3m8!1s0x883b2d4d71da8bb1:0xe4a8072514ccbe55!5m2!4m1!1i2!8m2!3d42.3388889!4d-83.0677778!16zL20vMDdtMmN3?entry=ttu"
-            target="_blank"
-            className="ml-16 cursor-pointer hover:underline"
-            rel="noreferrer"
-          >
-            <p className="xl:whitespace-nowrap">MotorCity Casino Hotel</p>
-            <p className="xl:whitespace-nowrap">
+          <div className="ml-16">
+            <h4 className="font-medium xl:whitespace-nowrap">
+              MotorCity Casino Hotel
+            </h4>
+            <address
+              className="not-italic xl:whitespace-nowrap"
+              aria-label="Event venue address"
+            >
               2901 Grand River Ave, Detroit, MI 48201
-            </p>
-          </a>
+            </address>
+            <a
+              href="https://www.google.com/maps/dir//2901+Grand+River+Ave,+Detroit,+MI+48201/@42.3388889,-83.0703527,1162m/data=!3m2!1e3!5s0x883b2d527a607d6f:0xe075cfc124b83793!4m20!1m10!3m9!1s0x883b2d4d71da8bb1:0xe4a8072514ccbe55!2sMotorCity+Casino+Hotel!5m2!4m1!1i2!8m2!3d42.3388889!4d-83.0677778!16zL20vMDdtMmN3!3m8!1s0x883b2d4d71da8bb1:0xe4a8072514ccbe55!5m2!4m1!1i2!8m2!3d42.3388889!4d-83.0677778!16zL20vMDdtMmN3?entry=ttu"
+              target="_blank"
+              className="mb-4 mt-3 inline-flex items-center rounded bg-sky-900 px-8 py-5 text-primary-400 shadow-xl transition delay-75 duration-100 ease-in-out hover:-translate-y-1 hover:scale-110 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-primary-400 focus:ring-offset-2"
+              rel="noreferrer"
+              aria-label="Get directions to MotorCity Casino Hotel (opens in new tab)"
+            >
+              GET DIRECTIONS
+            </a>
+          </div>
         </div>
 
         <div className="w-full lg:w-1/2">
