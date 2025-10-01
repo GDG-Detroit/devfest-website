df8dade91390436802d55fe3021c28aac115a509
Greg Miller <hello@gregmiller.io>
2025-10-01 16:10:11 -0400

    refactor: update NotFound.jsx
    - remove <main> boilerplate
    - add PageLayout
    - now consistent with other pages

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
+import { PageLayout } from '@/layouts/PageLayout'
 
 const NotFound = () => {
   useEffect(() => {
@@ -11,18 +12,20 @@ const NotFound = () => {
   }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gray-100 p-16 text-center">
-      <h1 id="main-heading" className="text-6xl font-bold text-primary-600">
-        404 - Page Not Found
-      </h1>
-      <p className="text-xl">
-        The page you were looking for does not exist, you will be redirected to
-        the home page in 5 seconds or you can click the link below.
-      </p>
-      <NavLink className="text-blue-500 hover:underline" to="/">
-        Return to Home Page
-      </NavLink>
-    </main>
+    <PageLayout>
+      <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gray-100 p-16 text-center">
+        <h1 id="main-heading" className="text-6xl font-bold text-primary-600">
+          404 - Page Not Found
+        </h1>
+        <p className="text-xl">
+          The page you were looking for does not exist, you will be redirected
+          to the home page in 5 seconds or you can click the link below.
+        </p>
+        <NavLink className="text-blue-500 hover:underline" to="/">
+          Return to Home Page
+        </NavLink>
+      </div>
+    </PageLayout>
   )
 }
 
