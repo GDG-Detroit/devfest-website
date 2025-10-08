55bc8e53b3a31dfbf4250bd30fc1bf532887105c
Greg Miller <hello@gregmiller.io>
2025-10-08 15:06:28 -0400

    refactor: fix name
    - update name based on whats app name

--- src/data/2025/dev.js
+++ src/data/2025/dev.js
@@ -3,7 +3,7 @@ import GregMiller from '@/data/2025/assets/images/dev/greg_miller.webp'
 import AaronAmano from '@/data/2025/assets/images/dev/aaron_amano.webp'
 import Rehanashri from '@/data/2025/assets/images/dev/rehanashri.webp'
 import ShugmiShumunov from '@/data/2025/assets/images/dev/shugmi_shumunov.webp'
-import KayleBusenbark from '@/data/2025/assets/images/dev/kayle_busenbark.webp'
+import KayleeBusenbark from '@/data/2025/assets/images/dev/kaylee_busenbark.webp'
 import JustinBeaudry from '@/data/2025/assets/images/dev/justin_beaudry.webp'
 
 export const devTeamData = [
@@ -52,12 +52,12 @@ export const devTeamData = [
     avatar: ShugmiShumunov,
   },
   {
-    name: 'Kayle Busenbark',
+    name: 'Kaylee Busenbark',
     github: 'https://github.com/kaybusenbark',
     university: 'University Of Michigan Dearborn',
     organization: '',
     position: 'Student',
-    avatar: KayleBusenbark,
+    avatar: KayleeBusenbark,
   },
   {
     name: 'Justin Beaudry',
