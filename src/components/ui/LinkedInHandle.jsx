c88e09ba8ff2f10c8758e57c198daf68378fef0f
Greg Miller <hello@gregmiller.io>
2025-10-19 22:19:37 -0400

    style: fix classname order

--- src/components/ui/LinkedInHandle.jsx
+++ src/components/ui/LinkedInHandle.jsx
@@ -8,7 +8,7 @@ const LinkedInHandle = ({ handle, absolute = true }) => (
     rel="noreferrer"
     className={
       absolute
-        ? 'absolute right-0 top-0 z-10 flex size-[34px] -translate-x-14 -translate-y-1/2 items-center justify-center rounded-full  bg-white'
+        ? 'absolute right-0 top-0 z-10 flex size-[34px] -translate-x-14 -translate-y-1/2 items-center justify-center rounded-full bg-white'
         : 'flex size-8 items-center justify-center rounded-full bg-white transition-colors hover:bg-gray-300'
     }
     aria-label="Follow on LinkedIn"
