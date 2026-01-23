22b4b2610ba548210dcec80e4593e1d2920d6467
Greg Miller <hello@gregmiller.io>
2026-01-23 17:29:06 -0500

    refactor: footer
    - remove dev credits for Shug and Shrinkray
    - add compass logo
    - modify layout

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,8 +1,7 @@
 import { Link } from 'react-router-dom'
 import { sections, externalLinks } from '../data/2025/navigation'
-import ShrinkrayLogo from '@/assets/images/icons/shrinkray.svg'
-import ShumunovLogo from '@/assets/images/icons/shug40.png'
 import GdgDetroitLogo from '@/assets/images/gdg-detroit-logo-footer.svg'
+import CompassDetroit from '@/assets/images/sponsors/Compass_Detroit_logo.webp'
 
 function Footer() {
   // Helper function to get navbar height for scroll offset
@@ -88,93 +87,38 @@ function Footer() {
       <div className="mx-auto w-full max-w-full border-t border-gray-300 bg-gray-900 px-6 pb-24 pt-8 lg:px-8 dark:border-gray-700">
         <div className="mx-auto max-w-7xl">
           <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
-            <div className="md:col-span-2">
+            <div className="col-span-1 md:col-span-3">
               {/* Left column - 2/3 width */}
               <div>
                 <h3 className="mb-2 text-lg font-semibold uppercase tracking-wide text-gray-400">
                   Brought to you by:
                 </h3>
               </div>
-              <div className="my-8 grid grid-cols-1 gap-6 text-white md:grid-cols-2">
-                <div className="flex flex-col items-center gap-2 md:flex-row md:items-center md:gap-4">
-                  <div className="flex justify-center md:mt-1">
-                    <img
-                      src={ShumunovLogo}
-                      alt="Shumunov Solutions logo"
-                      className="size-11 object-contain md:size-8"
-                      width={40}
-                      height={40}
-                    />{' '}
-                  </div>
-                  <div className="text-center md:mt-1 md:text-left">
-                    <p className="text-sm font-medium text-white">
-                      <a
-                        href="http://shumunovsolutions.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-base font-bold text-yellow-500 hover:text-gray-300"
-                      >
-                        Shumunov Solutions
-                      </a>
-                    </p>
-                    <p className="prose text-pretty text-sm text-white">
-                      UI/UX Development Fullstack React & Next.js
-                    </p>
-                    <p className="prose mt-2 text-pretty text-sm italic text-white">
-                      We blend thoughtful code with storytelling that truly
-                      connects.
-                    </p>
-                  </div>
-                </div>
-                <div className="flex flex-col items-center gap-2 md:flex-row md:items-center md:gap-4">
-                  <div className="flex justify-center">
-                    <img
-                      src={ShrinkrayLogo}
-                      alt="Shrinkray Interactive, LLC logo"
-                      className="size-16 object-contain"
-                      width={40}
-                      height={40}
-                    />{' '}
-                  </div>
-                  <div className="text-center md:mt-4 md:text-left">
-                    <p className="text-sm font-medium text-white">
-                      <a
-                        href="https://shrinkraylabs.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-base font-bold text-yellow-500 hover:text-gray-300"
-                      >
-                        Shrinkray Interactive, LLC
-                      </a>
-                    </p>
-                    <p className="prose text-pretty text-sm text-white">
-                      UI/UX Design, Accessibility, & Development
-                    </p>
-                    <p className="prose mt-2 text-pretty text-sm italic text-white">
-                      Enhancing Customer Experience through Accessibility,
-                      Performance, Project Management, and UX Engineering
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="w-full text-center italic text-gray-400 md:text-left">
-                ... just one damn project after another
-              </div>
             </div>
-            <div className="flex flex-col items-center justify-center md:col-span-1">
+            <div className="flex items-center justify-center">
               {/* Right column - 1/3 width */}
-              <div className="flex flex-col gap-1">
-                <img
-                  src={GdgDetroitLogo}
-                  alt="GDG Detroit Logo"
-                  className="h-auto w-64 object-contain object-center"
-                />
-                <p className="prose text-pretty text-sm text-white">
-                  The GDG Detroit team is a group of volunteers who are
-                  passionate about helping the community learn and grow in the
-                  field of technology.
-                </p>
-              </div>
+              <img
+                src={GdgDetroitLogo}
+                alt="GDG Detroit Logo"
+                className="h-auto w-64 object-contain object-center"
+              />
+            </div>
+            <div className="flex items-center justify-center">
+              {/* middle column - 1/3 width */}
+              <img
+                src={CompassDetroit}
+                alt="Compass Detroit Logo"
+                className="h-auto w-64 object-contain object-center"
+              />
+            </div>
+            <div className="grid grid-cols-1 items-center">
+              {/* Right column - 1/3 width */}
+
+              <p className="prose text-pretty text-sm text-white">
+                The Compass Detroit and GDG Detroit teams are volunteers who are
+                passionate about helping the community learn and grow in the
+                field of technology.
+              </p>
             </div>
           </div>
         </div>
